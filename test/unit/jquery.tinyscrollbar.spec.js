144b13e317102c9a76123324dde3fd821fa91217
Maarten Baijs <maartenbaijs@Maartens-MacBook-Pro.local>
2015-01-29 16:03:04 +0100

    Added new unit tests

--- test/unit/jquery.tinyscrollbar.spec.js
+++ test/unit/jquery.tinyscrollbar.spec.js
@@ -1,85 +1,3 @@
-/*jasmine.getFixtures().fixturesPath = "test/fixtures";
-
-describe("When jquery.tinyscrollbar is called ", function ()
-{
-    var $container  = null
-    ,   $container1 = null
-    ,   $container2 = null
-    ;
-
-    beforeEach(function()
-    {
-        loadFixtures("containers.html");
-
-        $container  = $(".container");
-        $container1 = $("#container1");
-        $container2 = $("#container2");
-    });
-
-    it("should be chainable.", function()
-    {
-        $container1.tinyscrollbar().addClass("test");
-
-        expect($container1).toHaveClass("test");
-    });
-
-    it("should create a seperate instance for each element in the HTMLCollection.", function()
-    {
-        $containers.tinyscrollbar();
-
-        expect($("#container").data("plugin_tinyscrollbar")).toBeTruthy();
-        expect($("#container2").data("plugin_tinyscrollbar")).toBeTruthy();
-    });
-
-    describe("without any options and there is content", function()
-    {
-        it("should have a style attr present on the scrollbar div.", function()
-        {
-            $container1.tinyscrollbar()
-
-            expect($container1.find("scrollbar")).toHaveAttr('style');
-        });
-    });
-
-    describe("with { thumbSize : 21 } and there is content", function()
-    {
-        it("should not have set thumb height to more then 21px.", function()
-        {
-            $container1.tinyscrollbar()
-
-            expect($container1.find("scrollbar")).toHaveAttr('style');
-        });
-    });
-
-    describe("with { trackSize : 50 } and there is content", function()
-    {
-        it("should not have set track height to more then 50px.", function()
-        {
-            $container1.tinyscrollbar()
-
-            expect($container1.find("scrollbar")).toHaveAttr('style');
-        });
-    });
-
-    describe("without any options and there is no content", function()
-    {
-        it("should have a disable class.", function()
-        {
-            $container1.tinyscrollbar()
-
-            expect($container1).toHaveClass("disable");
-        });
-
-        it("should not become active after a call to the update() method.", function()
-        {
-            $container1.tinyscrollbar()
-
-            expect($container1).toHaveClass("disable");
-        });
-    });
-
-});*/
-
 describe('A single Tinyscrollbar', function() {
     before(function () {
         document.head.innerHTML = __html__['test/fixtures/tinyscrollbar-css.html'];
@@ -105,4 +23,12 @@ describe('A single Tinyscrollbar', function() {
         expect(instance).to.be.a('object');
         expect(instance._name).to.equal('tinyscrollbar');
     });
+
+    it('should have a chainable update method', function() {
+        var instance = $('#scrollbar1').tinyscrollbar().data('plugin_tinyscrollbar');
+
+        expect(instance.update()._name).to.equal('tinyscrollbar');
+    });
+
+
 });
--- test/unit/tinyscrollbar.spec.js
+++ test/unit/tinyscrollbar.spec.js
@@ -1,50 +1,3 @@
-/*jasmine.getFixtures().fixturesPath = "test/fixtures";
-
-describe("tinyscrollbar", function ()
-{
-    var $container  = null
-    ,   $container1 = null
-    ,   $container2 = null
-    ;
-
-    beforeEach(function()
-    {
-        loadFixtures("containers.html");
-
-        $container  = $(".container");
-        $container1 = $("#container1");
-        $container2 = $("#container2");
-    });
-
-    it("should be chainable.", function()
-    {
-        $container1.tinyscrollbar().addClass("test");
-
-        expect($container1).toHaveClass("test");
-    });
-
-    it("should work on more then one container simultaneously.", function()
-    {
-        $containers.tinyscrollbar();
-
-        expect($("#container").data("plugin_tinyscrollbar")).toBeTruthy();
-        expect($("#container2").data("plugin_tinyscrollbar")).toBeTruthy();
-    });
-
-    it("should be able to handle (async) content updates.", function()
-    {
-        $container1.tinyscrollbar();
-
-
-        $container1.data("plugin_tinyscrollbar")
-
-        expect($container1).toHaveClass("test");
-    });
-
-});
-*/
-
-
 describe('A single Tinyscrollbar', function() {
     before(function () {
         document.head.innerHTML = __html__['test/fixtures/tinyscrollbar-css.html'];
@@ -57,4 +10,12 @@ describe('A single Tinyscrollbar', function() {
     afterEach(function () {
         document.body.innerHTML = '';
     });
+
+    it('should have a accessible instance', function() {
+        var instance = tinyscrollbar(document.getElementById("scrollbar1"));
+
+        expect(instance).to.be.a('object');
+        expect(instance._name).to.equal('tinyscrollbar');
+    });
+
 });
